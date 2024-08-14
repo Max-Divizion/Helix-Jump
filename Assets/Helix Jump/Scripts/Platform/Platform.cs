148cf1d708d510b5ff238d5ea7f4157ca59eaf6e
Max_Divizion <99737414+Max-Divizion@users.noreply.github.com>
2024-08-14 17:16:21 +0300

    First commit

---
+++ Assets/Helix Jump/Scripts/Platform/Platform.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private float _bounceForce;
+    [SerializeField] private float _bounceRadius;
+
+    public void Break()
+    {
+        PlatformSegment[] platformSegments = GetComponentsInChildren<PlatformSegment>();
+
+        foreach (var segment in platformSegments)
+        {
+            segment.Bounce(_bounceForce, transform.position, _bounceRadius);
+        }
+    }
+
+}
